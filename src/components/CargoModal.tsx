0c8ecccbd0099ade61d39cef7afa75b0abe68d93
Vadim <a-yva-2013@mail.ru>
2025-04-01 23:42:49 +0400

    Доработка загрузки

--- src/components/CargoModal.tsx
+++ src/components/CargoModal.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useRef, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -54,6 +55,16 @@ const CargoModal: React.FC<CargoModalProps> = ({
   // Режим редактирования?
   const isEditMode = Boolean(cargo);
 
+  // Состояние для файлов
+  const [files, setFiles] = useState<File[]>([]);
+  // Реф для скрытого input
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  // Функция для удаления файла из списка
+  const handleRemoveFile = (index: number) => {
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+  };
+
   // Начальное состояние для формы
   const [formData, setFormData] = useState<AddCargo>({
     cargoNumber: "",
@@ -69,6 +80,14 @@ const CargoModal: React.FC<CargoModalProps> = ({
   });
 
   const [errors, setErrors] = useState<Record<string, string>>({});
+
+  // Функция для обработки выбора файлов
+  const handleFilesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const filesArray = Array.from(e.target.files);
+      setFiles((prev) => [...prev, ...filesArray]);
+    }
+  };
 
   useEffect(() => {
     if (cargo) {
@@ -187,7 +206,7 @@ const CargoModal: React.FC<CargoModalProps> = ({
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[600px]">
+      <DialogContent className="sm:max-w-[650px] overflow-auto h-[600px]">
         <DialogHeader>
           <DialogTitle>
             {isEditMode ? "Редактировать груз" : "Добавить груз"}
@@ -312,83 +331,87 @@ const CargoModal: React.FC<CargoModalProps> = ({
             />
           </div>
 
-          {/* Водитель */}
-          <div>
-            <Label htmlFor="driver">Водитель</Label>
-            <Input
-              id="driver"
-              type="text"
-              value={formData.driver}
-              onChange={(e) =>
-                setFormData({ ...formData, driver: e.target.value })
-              }
-            />
+          <div className="flex items-center gap-4">
+            {/* Водитель */}
+            <div className="flex flex-col gap-1 w-full">
+              <Label htmlFor="driver">Водитель</Label>
+              <Input
+                id="driver"
+                type="text"
+                value={formData.driver}
+                onChange={(e) =>
+                  setFormData({ ...formData, driver: e.target.value })
+                }
+              />
+            </div>
+
+            {/* Сумма выплаты */}
+            <div className="flex flex-col gap-1 w-full">
+              <Label htmlFor="payoutAmount">Сумма выплаты</Label>
+              <Input
+                id="payoutAmount"
+                type="number"
+                value={formData.payoutAmount}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    payoutAmount: Number(e.target.value),
+                  })
+                }
+              />
+            </div>
           </div>
 
-          {/* Сумма выплаты */}
-          <div>
-            <Label htmlFor="payoutAmount">Сумма выплаты</Label>
-            <Input
-              id="payoutAmount"
-              type="number"
-              value={formData.payoutAmount}
-              onChange={(e) =>
-                setFormData({
-                  ...formData,
-                  payoutAmount: Number(e.target.value),
-                })
-              }
-            />
-          </div>
+          <div className="flex items-center gap-4">
+            {/* Дата выплаты */}
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="payoutDate">Дата выплаты</Label>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-[240px] justify-start text-left font-normal",
+                      !formData.payoutDate && "text-muted-foreground"
+                    )}
+                  >
+                    {formData.payoutDate
+                      ? format(formData.payoutDate, "dd.MM.yyyy")
+                      : "Выберите дату"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    locale={{
+                      ...ru,
+                      options: {
+                        ...ru.options,
+                        weekStartsOn: 1,
+                      },
+                    }}
+                    selected={formData.payoutDate ?? new Date()}
+                    onSelect={(day: Date | undefined) => {
+                      if (!day) return;
+                      setFormData({ ...formData, payoutDate: day });
+                    }}
+                  />
+                </PopoverContent>
+              </Popover>
+            </div>
 
-          {/* Дата выплаты */}
-          <div className="flex flex-col gap-1">
-            <Label htmlFor="payoutDate">Дата выплаты</Label>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant={"outline"}
-                  className={cn(
-                    "w-[240px] justify-start text-left font-normal",
-                    !formData.payoutDate && "text-muted-foreground"
-                  )}
-                >
-                  {formData.payoutDate
-                    ? format(formData.payoutDate, "dd.MM.yyyy")
-                    : "Выберите дату"}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                  mode="single"
-                  locale={{
-                    ...ru,
-                    options: {
-                      ...ru.options,
-                      weekStartsOn: 1,
-                    },
-                  }}
-                  selected={formData.payoutDate ?? new Date()}
-                  onSelect={(day: Date | undefined) => {
-                    if (!day) return;
-                    setFormData({ ...formData, payoutDate: day });
-                  }}
-                />
-              </PopoverContent>
-            </Popover>
-          </div>
-
-          {/* Статус оплаты */}
-          <div>
-            <Label htmlFor="paymentStatus">Статус оплаты</Label>
-            <Input
-              id="paymentStatus"
-              type="text"
-              value={formData.paymentStatus}
-              onChange={(e) =>
-                setFormData({ ...formData, paymentStatus: e.target.value })
-              }
-            />
+            {/* Статус оплаты */}
+            <div className="flex flex-col gap-1 w-full">
+              <Label htmlFor="paymentStatus">Статус оплаты</Label>
+              <Input
+                id="paymentStatus"
+                type="text"
+                value={formData.paymentStatus}
+                onChange={(e) =>
+                  setFormData({ ...formData, paymentStatus: e.target.value })
+                }
+              />
+            </div>
           </div>
 
           {/* Условия выплаты */}
@@ -404,29 +427,89 @@ const CargoModal: React.FC<CargoModalProps> = ({
             />
           </div>
 
-          {trucks && (
-            <div>
-              <Label htmlFor="truckId">Выберите машину</Label>
-              <Select
-                value={formData.truckId}
-                onValueChange={(value) =>
-                  setFormData({ ...formData, truckId: value })
-                }
-              >
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Выберите машину" />
-                </SelectTrigger>
-                <SelectContent>
-                  {trucks.map((truck) => (
-                    <SelectItem key={truck.id} value={String(truck.id)}>
-                      {truck.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              {errors.truckId && (
-                <p className="text-red-500 text-sm">{errors.truckId}</p>
-              )}
+          <div className="flex items-center gap-4">
+            {trucks && (
+              <div className="w-full">
+                <Label htmlFor="truckId">Выберите машину</Label>
+                <Select
+                  value={formData.truckId}
+                  onValueChange={(value) =>
+                    setFormData({ ...formData, truckId: value })
+                  }
+                >
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Выберите машину" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {trucks.map((truck) => (
+                      <SelectItem key={truck.id} value={String(truck.id)}>
+                        {truck.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                {errors.truckId && (
+                  <p className="text-red-500 text-sm">{errors.truckId}</p>
+                )}
+              </div>
+            )}
+
+            <div className="flex flex-col gap-1 w-full">
+              <Label htmlFor="files">
+                Прикрепить файлы (png, jpg, pdf, Excel)
+              </Label>
+              {/* Кнопка для открытия диалога выбора файлов */}
+              <Button onClick={() => fileInputRef.current?.click()}>
+                Загрузить файлы
+              </Button>
+              {/* Скрытый input для файлов */}
+              <input
+                id="files"
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                multiple
+                accept="image/png, image/jpeg, application/pdf, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                onChange={handleFilesChange}
+              />
+            </div>
+          </div>
+
+          {files.length > 0 && (
+            <div className="mt-4">
+              <p className="mb-2 font-semibold">
+                Предпросмотр загруженных файлов:
+              </p>
+              <div className="flex flex-wrap gap-4">
+                {files.map((file, index) => {
+                  const isImage = file.type.startsWith("image/");
+                  return (
+                    <div
+                      key={index}
+                      className="rounded relative"
+                      style={{ width: 120, height: 120 }}
+                    >
+                      {isImage ? (
+                        <img
+                          src={URL.createObjectURL(file)}
+                          alt={file.name}
+                          className="object-cover w-full h-full"
+                        />
+                      ) : (
+                        <div className="flex items-center justify-center w-full h-full text-center text-sm">
+                          {file.name}
+                        </div>
+                      )}
+                      <Button
+                        onClick={() => handleRemoveFile(index)}
+                        className="absolute flex top-1 bg-red-500 text-[12px] right-1 size-[16px] p-0 flex-1"
+                      >
+                        ✕
+                      </Button>
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           )}
         </div>
