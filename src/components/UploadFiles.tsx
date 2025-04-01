715393c578f5482e014a1b3a727d0e9263aa3af1
Vadim <a-yva-2013@mail.ru>
2025-04-01 23:44:55 +0400

    Доработка загрузки

--- src/components/UploadFiles.tsx
+++ src/components/UploadFiles.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useRef } from "react";
 import { Trash, UploadCloud } from "lucide-react";
 import Image from "next/image";
