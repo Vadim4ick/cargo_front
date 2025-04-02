b0b77054e6d2fe7c753389cd48e3640a82da1a4e
Vadim Firulyov <firulvv@mail.ru>
2025-04-02 15:09:50 +0400

    Изменена логика по логину, добален хук

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,19 @@
+import { authServices } from "@/services/auth.service";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+const useLogin = () => {
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      authServices.login({ email, password }),
+    onSuccess: () => {
+      router.push("/");
+    },
+  });
+
+  return { mutate, isPending };
+};
+
+export { useLogin };
